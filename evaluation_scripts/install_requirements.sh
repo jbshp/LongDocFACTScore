d6e280fefb40384a063e4a336f9be1c462b4b8d7
Jennifer Bishop <jennybishop2001@yahoo.co.uk>
2024-05-24 06:50:57 +0200

    update files no longer needed

--- evaluation_scripts/install_requirements.sh
+++
@@ -1,4 +0,0 @@
-pip install -r evaluation_scripts/requirements.txt
-pip install -r evaluation_scripts/factCC/requirements.txt
-pip install -r evaluation_scripts/BARTScore/requirements.txt
-pip install -r evaluation_scripts/QuestEval/requirements.txt
